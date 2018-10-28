244e0c432bf5a1292edd15e49e18fd3fb91861cc
Jeff-Destop <jeffreymj94@gmail.com>
2018-10-28 15:21:52 -0700

    modify Register.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import {TopBar} from './TopBar'
+import {Register} from './Register'
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <TopBar />
+        <Register />
       </div>
     );
   }
